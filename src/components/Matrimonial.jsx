fe05e5490ef827f392d76ee1b0befa837909d803
vinod4848 <vinodku4848@gmail.com>
2024-08-29 18:34:17 +0530

    first commit

---
+++ src/components/Matrimonial.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Matrimonial = (props) => {
+  return (
+    <div id="Matrimonial" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Matrimony</h2>
+          <p>
+            Stay updated with our latest events and join us for exciting
+            experiences.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
